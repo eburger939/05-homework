12bcbdc12c5aee5a308dcfcc3b02176ec35b0f40
Emily Burger <eburger939@gmail.com>
2021-12-16 08:20:48 -0600

    running correctly, just need readme

--- assets/script2.js
+++ assets/script2.js
@@ -1,48 +1,47 @@
 
-var saveBtn = document.querySelector('.saveBtn');
 
 function displayTime() {
     var currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     currentDay.textContent = currentTime;
 }
 
-// function timeTracker(){
-    // var timeNow = moment().hours();
-    // $(".time-block").each(function (){
-    // var columnTime = parseInt($(this).attr('id').split("")[1]);
-    // console.log(columnTime)
+function timeTracker(){
+    var timeNow = moment().hour();
+    $(".time-block").each(function (){
+    var columnTime = parseInt($(this).attr("id").split("time")[1]);
+    console.log(columnTime, timeNow)
+    
+    if (columnTime > timeNow) {
+        $(this).children().eq(1).addClass("future")
+    } else if (columnTime < timeNow) {
+        $(this).children().eq(1).addClass("past")
+    } else {
+        $(this).children().eq(1).addClass("present")
+    }
+})
+}
 
-    // if (timeNow > columnTime) {
-        // $(this).addClass("past")
-    // } else if (timeNow < columnTime) {
-        // $(this).addClass("future")
-    // } else {
-        // $(this).addClass("present")
-    // }
-// });
-// }
-
-
-function addAppt(event) {
-    var apptTime = $(this).attr("id")
-    var submission = $(this).parent().children().siblings(".form-control").val()
+$('.saveBtn').on("click", function() {
+    var apptTime = $(this).parent().attr("id")
+    var submission = $(this).siblings(".form-control").val()
     localStorage.setItem(apptTime, submission);
     console.log(apptTime);
     console.log(submission);
-}
-
-$("#nine").val(localStorage.getItem('09'))
-$("#ten").val(localStorage.getItem('10'))
-$("#eleven").val(localStorage.getItem('11'))
-$("#twelve").val(localStorage.getItem('12'))
-$("#thirteen").val(localStorage.getItem('13'))
-$("#fourteen").val(localStorage.getItem('14'))
-$("#fifteen").val(localStorage.getItem('15'))
-$("#sixteen").val(localStorage.getItem('16'))
-$("#seventeen").val(localStorage.getItem('17'))
+});
 
 
-saveBtn.addEventListener("click", addAppt)
+$("#time9 .form-control").val(localStorage.getItem("time9"))
+$("#time10 .form-control").val(localStorage.getItem("time10"))
+$("#time11 .form-control").val(localStorage.getItem("time11"))
+$("#time12 .form-control").val(localStorage.getItem("time12"))
+$("#time13 .form-control").val(localStorage.getItem("time13"))
+$("#time14 .form-control").val(localStorage.getItem("time14"))
+$("#time15 .form-control").val(localStorage.getItem("time15"))
+$("#time16 .form-control").val(localStorage.getItem("time16"))
+$("#time17 .form-control").val(localStorage.getItem("time17"))
+
+
 
 
 setInterval (displayTime, 1000);
+timeTracker();
