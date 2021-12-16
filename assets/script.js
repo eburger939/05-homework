a0cdf0639b0d2c7ef08842008b58008b1fecd979
Emily Burger <eburger939@gmail.com>
2021-12-15 20:46:31 -0600

    added number variables

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
+//variables for the date
+var currentDay = $('#currentDay');
+var todaysDate = moment().format("")
 
+//variables for text are
+var textArea = $('.form-control');
+var nine = $("#9");
+var ten = $("#10");
+var eleven = $("#11");
+var twelve = $("#12");
+var thirteen = $("#13");
+var fourteen = $("#14");
+var fifteen = $("#15");
